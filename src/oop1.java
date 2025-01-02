6c3e9324ba95753a0db9e97692605203e878547e
premcopper <premborphale@gmail.com>
2025-01-02 18:49:44 +0530

    commit

---
+++ src/oop1.java
@@ -0,0 +1,13 @@
+public class oop1{
+    int x=70;
+    String str1="prem";
+    static String str2="oop";
+    String str3=new String("jjjj");
+
+    public static void main(String[] args) {
+        oop1 obj=new oop1();
+        System.out.println(obj.str1);
+        System.out.println(obj.str2);
+        System.out.println(obj.str3);
+    }
+}
---
+++ src/oop2.java
@@ -0,0 +1,14 @@
+public class oop2 {
+    int x=70;
+    String str1="prem";
+    static String str2="oop";
+    String str3=new String("jjjj");
+
+    public static void main(String[] args) {
+        oop2 obj=new oop2();
+        System.out.println(obj.str1);
+        System.out.println(obj.str2);
+        System.out.println(obj.str3);
+    }
+}
+
