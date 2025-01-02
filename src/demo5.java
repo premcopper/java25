e4b31da8ff32e04eb27f66718339ca89b3922f2d
premcopper <premborphale@gmail.com>
2025-01-02 18:48:23 +0530

    commit

---
+++ src/demo5.java
@@ -0,0 +1,15 @@
+class demo5 {
+
+    public static void main(String[] args){
+
+        String str1 = "Shashi";
+        System.out.println(str1);
+        System.out.println(System.identityHashCode(str1));
+
+        String str2 = new String("Shashi");
+        System.out.println(str2);
+        System.out.println(System.identityHashCode(str2));
+
+
+    }
+}
