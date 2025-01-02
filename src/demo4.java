af6adfe5281ed2ef3a2d4eb535bbd297c0a08a5c
premcopper <premborphale@gmail.com>
2025-01-02 18:46:43 +0530

    commit

---
+++ src/demo4.java
@@ -0,0 +1,12 @@
+class demo4 {
+
+    public static void main(String[] args){
+
+        String str1 = "Shashi";
+        System.out.println(str1);
+
+        char arr[] = new char[]{'a','b','c','d','e','f'};
+        System.out.println(arr);
+    }
+}
+
