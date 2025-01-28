31924ba667f9b253770adb132bf8d23787f05af8
premcopper <premborphale@gmail.com>
2025-01-28 22:57:15 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/oop3.java
@@ -0,0 +1,14 @@
+public class oop3  {
+    int x=70;
+    String str1="prem";
+    static String str2="oop";
+    String str3=new String("jjjj");
+
+    public static void main(String[] args) {
+        oop3 obj=new oop3();
+        System.out.println(obj.str1);
+        System.out.println(obj.str2);
+        System.out.println(obj.str3);
+    }
+}
+}
